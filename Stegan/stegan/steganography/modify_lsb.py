3d1d167405840ff219ec2e1c1f4f132138ccecf7
Robert O'Neill <rjroneill@gmail.com>
2012-02-15 17:27:41 -0500

    Added spacing between payload data in trojan file

--- Stegan/stegan/steganography/modify_lsb.py
+++ Stegan/stegan/steganography/modify_lsb.py
@@ -1,27 +1,7 @@
-import sys
+import sys, math
 
 from stegan.payload import Payload
 from stegan.audio.wavefile import WaveFile
-
-def testBit(int_type, offset):
-    """Return a non-zero result, 2 ** offset, if the bit at 'offset' is one"""
-    mask = 1 << offset
-    return(int_type & mask)
-
-def set_bit(int_type, offset):
-    """Return an integer with the bit at 'offset' set to 1"""
-    mask = 1 << offset
-    return(int_type | mask)
-
-def clear_bit(int_type, offset):
-    """Returns an integer with the bit at 'offset' cleared (set to 0)"""
-    mask = ~(1 << offset)
-    return(int_type & mask)
-
-def toggle_bit(int_type, offset):
-    """Returns an integer with the bit at 'offset' inverted. 0 -> 1 and 1-> 0"""
-    mask = 1 << offset
-    return(int_type ^ mask)
 
 class BitArray(object):
     
@@ -107,14 +87,22 @@ def encode(payload, container):
     payload_size_in_bits = payload_size * 8
     payload_size_bits = BitArray(payload_size, size=32)
 
+    # Find a spacing width
+    payload_spacing = int(math.floor((len(container.data) - 32) / payload_size_in_bits))
+
+    print "[modify_lsb] encode - payload_spacing = %s" % payload_spacing
+    
     print "[modify_lsb] encode - payload_size = %s" % payload_size
     print "[modify_lsb] encode - len(payload_size_bits) = %s" % len(payload_size_bits)
     print "[modify_lsb] encode - payload_size_bits = %s" % ''.join([str(b) for b in payload_size_bits])
     
-    print "[modify_lsb] bits written"
+    print "[modify_lsb] bits written"    
+
+    paybit_idx = 0
+    paybit_space = 0
     
     for cidx, cbyte in enumerate(container.data):
-
+        
         # First 32 bytes of the trojan will have the file size of the 
         # payload encoded in their lsb. [0, 31]
         if 0 <= cidx <= 31:
@@ -126,22 +114,22 @@ def encode(payload, container):
         # Skip writing any trojan data in byte at index 32
         elif cidx == 32:
             tbyte = cbyte
-            
-        # Trojan bytes [33, n] will have the payload encoded in their lsbs,
-        # where n is payload_size * 8.
+
         elif 33 <= cidx:
-            paybit_idx = (cidx - 33)
+            #paybit_idx = (cidx - 33)
             
-            if paybit_idx < payload_size_in_bits:
+            if paybit_idx < payload_size_in_bits and paybit_space == payload_spacing:
                 payload_bit = get_bit(payload.data, paybit_idx)
-                
-                #print "paybit is %s, payload_bit is %s" % (paybit_idx, payload_bit)
-                sys.stdout.write("%s" % payload_bit)
 
                 tbyte = set_lsb(cbyte, payload_bit)
+
+                paybit_idx += 1
+                paybit_space = 0
             else:
                 tbyte = cbyte
-            
+                
+            paybit_space += 1    
+                
         trojan_data.append(tbyte)
         
     print ""
@@ -155,6 +143,11 @@ def decode(trojan):
     payload_size = 0
     
     paybit_stack = []
+
+    bits_read = 0
+
+    payload_spacing = 0
+    paybit_space = 0
     
     for tidx, tbyte in enumerate(trojan.data):
         
@@ -164,12 +157,16 @@ def decode(trojan):
             paysize_bit = get_lsb(tbyte)
             payload_size_bits.append(paysize_bit)
             
-            sys.stdout.write("%s" % paysize_bit)
+            #sys.stdout.write("%s" % paysize_bit)
             
         # There is no payload data at trojan byte index 32. Let's compute
         # the integer value of the payload_size here for use later.
         elif tidx == 32:
             payload_size = BitArray.from_bits(payload_size_bits).to_int()
+
+            payload_spacing = int(math.floor((len(trojan.data) - 32) / (payload_size * 8)))
+
+            print "Payload spacing is %s" % payload_spacing
             
             print "[modify_lsb] decode - payload_size = %s" % payload_size
             print "[modify_lsb] decode - payload_size_bits = %s" % \
@@ -179,22 +176,31 @@ def decode(trojan):
         # Trojan bytes [33, n] will have the payload encoded in their lsbs,
         # where n is payload_size * 8, where payload_size is the number of 
         # bytes in payload.
-        elif 33 <= tidx <= (33 + (payload_size * 8)):
-            payload_bit = get_lsb(tbyte)
 
-            sys.stdout.write("%s" % payload_bit)            
-            paybit_stack.append(payload_bit)
-            
+        # this will no longer be bounded by the size of the payload data
+        # it will be bounded by the size of the trojan data
+        elif 33 <= tidx:
+
+            if paybit_space == payload_spacing:
+                payload_bit = get_lsb(tbyte)
+                
+                paybit_stack.append(payload_bit)
+                
+                paybit_space = 0
+                
             # We've read enough bits to make a byte. DO IT.
             if len(paybit_stack) == 8:
                 paybyte = BitArray.from_bits(paybit_stack).to_int()
                 payload_data.append(paybyte)
                 paybit_stack = []
-            
+                
+            paybit_space += 1    
+                
         # There's no more payload data to be read from the trojan, let's break
         else:
+            print "Bits read is %s" % bits_read
+            print "Out of %s" % payload_size
             break
-        
         
     print ""
     
